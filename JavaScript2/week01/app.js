96b6a554560829cc0f68fbf1b7171f20a3d198dc
orfeika <tolstenkova.a.p@gmail.com>
2018-10-24 16:42:45 +0200

    Added shelfs and book covers

--- JavaScript2/week01/app.js
+++ JavaScript2/week01/app.js
@@ -1,10 +1,4 @@
 ﻿"use strict";
-let booksList = ['EatPrayLove', 'TheLittlePrince' , 
-				'TheGreatGatsby' , 'Fahrenheit451',
-				'PrideAndPrejudice','ToKillAMockingbird ',
-				'AllQuietOnTheWesternFront', 'WarAndPeace',
-				'TheCatcherInTheRye','TheHitchhikersGuideToTheGalaxy',
-				'ASongOfIceandFire'];
 
 //Eat Pray Love by Elizabeth Gilber
 //The Little Prince by Antoine de Saint-Exupéry
@@ -17,116 +11,70 @@ let booksList = ['EatPrayLove', 'TheLittlePrince' ,
 //The Hitchhiker's Guide to the Galaxy Douglas Adams
 //	A Song of Ice and Fire George R. R. Martin
 
-function Book(author, title, language) {
+function Book(author, title, language, cover) {
   this.author = author;
   this.title = title;
   this.language = language;
+  this.cover = cover; 
 } 
 
 
 
 
 
-
-let aSongOfIceandFire = {
-author: "George R. R. Martin" ,
-title:"A Song of Ice and Fire",
-language:"English"
-
-};
-
-let theHitchhikersGuideToTheGalaxy = {
-author:"Douglas Adams",
-title:"The Hitchhiker's Guide to the Galaxy Douglas",
-language: "English"
-
-};
-
-let theCatcherInTheRye = {
-author:"J.D. Salinger",
-title:"The Catcher in the Rye ",
-language: "English"
-
-};
-
-let warAndPeace = {
-author:"Leo Tolstoy",
-title:"War and Peace",
-language: "English"
-
-};
+let booksList = ['EatPrayLove', 'TheLittlePrince' , 
+				'TheGreatGatsby' , 'Fahrenheit451',
+				'PrideAndPrejudice','ToKillAMockingbird ',
+				'AllQuietOnTheWesternFront', 'WarAndPeace',
+				'TheCatcherInTheRye','TheHitchhikersGuideToTheGalaxy',
+				'ASongOfIceandFire'];
 
 
-let allQuietOnTheWesternFront = {
-author:"Erich Marie Remarque",
-title:"All Quiet on the Western Front",
-language: "English"
-};
+let bookObjectList = [new Book ("Elizabeth Gilber","Eat Pray Love ","English" ,"covers/EatPrayLove.jpg" ) , 
+					new Book("Antoine de Saint-Exupéry","The Little Prince", "English","covers/TheLittlePrince.jpg" ),
+					new Book("F.Scott Fitzgerald","The Great Gatsby","English", "covers/TheGreatGatsby.jpg"),
+					new Book("Ray Bradbury"," Fahrenheit 451","English", "covers/Fahrenheit451.jpg"),
+					new Book("Jane Austen","Pride and Prejudice", "English", "covers/PrideAndPrejudice.jpg"),
+					new Book("Harper Lee","To Kill a Mockingbird", "English","covers/ToKillAMockingbird.jpg"),
+					new Book ("Erich Marie Remarque","All Quiet on the Western Front","English","covers/AllQuietOnTheWesternFront.jpg"),
+					new Book("Leo Tolstoy","War and Peace","English", "covers/WarAndPeace.jpg"),
+					new Book("J.D. Salinger","The Catcher in the Rye ","English", "covers/TheCatcherInTheRye.jpg"),
+					new Book("Douglas Adams","The Hitchhiker's Guide to the Galaxy Douglas","English","covers/TheHitchhikersGuideToTheGalaxy.jpg"),
+					new Book( "George R. R. Martin" ,"A Song of Ice and Fire","English","covers/ASongOfIceandFire.jpg")];
 
-let toKillAMockingbird = {
-author:"Harper Lee",
-title:"To Kill a Mockingbird",
-language: "English"
-};
-
-let prideAndPrejudice = {
-author:"Jane Austen",
-title:"Pride and Prejudice",
-language: "English"
-};
-
-let fahrenheit451 = {
-author:"by Ray Bradbury",
-title:" Fahrenheit 451",
-language:"English"
-};
-
-let theGreatGatsby = {
-author:"F.Scott Fitzgerald",
-title:"The Great Gatsby",
-language: "English"
-};
-
-
-let theLittlePrince = {
-author:"Antoine de Saint-Exupéry",
-title:"The Little Prince",
-language: "English"
-};
-
-let eatPrayLove = {
-author:"Elizabeth Gilber",
-title:"Eat Pray Love ",
-language: "English"
-};
-
-
-let bookObjectList = [new Book( "George R. R. Martin" ,
-								"A Song of Ice and Fire",
-								"English"), 
-					new Book("Douglas Adams",
-							"The Hitchhiker's Guide to the Galaxy Douglas",
-							"English"),
-					theGreatGatsby, fahrenheit451, 
-					prideAndPrejudice, toKillAMockingbird, 
-					allQuietOnTheWesternFront,warAndPeace,
-					theCatcherInTheRye, theHitchhikersGuideToTheGalaxy,
-					aSongOfIceandFire];
-
-
+ 
 function generateElements(){
         let ul = document.createElement('ul');
 		 ul.setAttribute('id','booksList');
 	 document.getElementById('bookShelf').appendChild(ul);
 	 booksList.forEach(renderBookList);
+	 bookObjectList.forEach(createBooks);
+
 
 	  function renderBookList(element, index, array){
 	 	let li  = document.createElement('li');
 		li.setAttribute('class','book');
 		 ul.appendChild(li);
 		li.setAttribute('id', element);
-		
 	 }
+
+	 function createBooks(element, index, array){
+		 let liList = document.getElementsByClassName("book");
+		 let div = document.createElement('div');
+		 div.setAttribute('class' , 'books');
+		 let divBookTitle = document.createElement('div');
+		 divBookTitle.setAttribute('class', 'title')
+		 let p = document.createElement('p');
+		 let img = document.createElement('img');
+		 img.setAttribute('src',element.cover);
+		 let mainDiv= liList[index].appendChild(div);
+		 mainDiv.appendChild(divBookTitle).appendChild(p);
+		 mainDiv.append(img);	
+		 p.innerHTML =p.innerHTML + " " +element.author + " " + element.title ;
+	 }
+
+
+
 
 
 }
