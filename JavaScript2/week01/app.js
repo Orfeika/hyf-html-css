85904e0f135b57bc121d186c7949842c5feed8da
orfeika <tolstenkova.a.p@gmail.com>
2018-10-25 09:49:56 +0200

    added styles

--- JavaScript2/week01/app.js
+++ JavaScript2/week01/app.js
@@ -11,7 +11,8 @@
 //The Hitchhiker's Guide to the Galaxy Douglas Adams
 //	A Song of Ice and Fire George R. R. Martin
 
-function Book(author, title, language, cover) {
+function Book(id, author, title, language, cover) {
+  this.id = id; 		
   this.author = author;
   this.title = title;
   this.language = language;
@@ -22,55 +23,58 @@ function Book(author, title, language, cover) {
 
 
 
-let booksList = ['EatPrayLove', 'TheLittlePrince' , 
-				'TheGreatGatsby' , 'Fahrenheit451',
-				'PrideAndPrejudice','ToKillAMockingbird ',
-				'AllQuietOnTheWesternFront', 'WarAndPeace',
-				'TheCatcherInTheRye','TheHitchhikersGuideToTheGalaxy',
-				'ASongOfIceandFire'];
 
 
-let bookObjectList = [new Book ("Elizabeth Gilber","Eat Pray Love ","English" ,"covers/EatPrayLove.jpg" ) , 
-					new Book("Antoine de Saint-Exupéry","The Little Prince", "English","covers/TheLittlePrince.jpg" ),
-					new Book("F.Scott Fitzgerald","The Great Gatsby","English", "covers/TheGreatGatsby.jpg"),
-					new Book("Ray Bradbury"," Fahrenheit 451","English", "covers/Fahrenheit451.jpg"),
-					new Book("Jane Austen","Pride and Prejudice", "English", "covers/PrideAndPrejudice.jpg"),
-					new Book("Harper Lee","To Kill a Mockingbird", "English","covers/ToKillAMockingbird.jpg"),
-					new Book ("Erich Marie Remarque","All Quiet on the Western Front","English","covers/AllQuietOnTheWesternFront.jpg"),
-					new Book("Leo Tolstoy","War and Peace","English", "covers/WarAndPeace.jpg"),
-					new Book("J.D. Salinger","The Catcher in the Rye ","English", "covers/TheCatcherInTheRye.jpg"),
-					new Book("Douglas Adams","The Hitchhiker's Guide to the Galaxy Douglas","English","covers/TheHitchhikersGuideToTheGalaxy.jpg"),
-					new Book( "George R. R. Martin" ,"A Song of Ice and Fire","English","covers/ASongOfIceandFire.jpg")];
+
+let bookObjectList = [new Book ('EatPrayLove',"Elizabeth Gilber","Eat Pray Love ","English" ,"covers/EatPrayLove.jpg" ) , 
+					new Book('TheLittlePrince' ,"Antoine de Saint-Exupéry","The Little Prince", "English","covers/TheLittlePrince.jpg" ),
+					new Book('TheGreatGatsby',"F.Scott Fitzgerald","The Great Gatsby","English", "covers/TheGreatGatsby.jpg"),
+					new Book('Fahrenheit451',"Ray Bradbury"," Fahrenheit 451","English", "covers/Fahrenheit451.jpg"),
+					new Book('PrideAndPrejudice',"Jane Austen","Pride and Prejudice", "English", "covers/PrideAndPrejudice.jpg"),
+					new Book('ToKillAMockingbird ',"Harper Lee","To Kill a Mockingbird", "English","covers/ToKillAMockingbird.jpg"),
+					new Book ('AllQuietOnTheWesternFront',"Erich Marie Remarque","All Quiet on the Western Front","English","covers/AllQuietOnTheWesternFront.jpg"),
+					new Book('WarAndPeace',"Leo Tolstoy","War and Peace","English", "covers/WarAndPeace.jpg"),
+					new Book('TheCatcherInTheRye',"J.D. Salinger","The Catcher in the Rye ","English", "covers/TheCatcherInTheRye.jpg"),
+					new Book('TheHitchhikersGuideToTheGalaxy',"Douglas Adams","The Hitchhiker's Guide to the Galaxy Douglas","English","covers/TheHitchhikersGuideToTheGalaxy.jpg"),
+					new Book('ASongOfIceandFire', "George R. R. Martin" ,"A Song of Ice and Fire","English","covers/ASongOfIceandFire.jpg")];
 
  
 function generateElements(){
         let ul = document.createElement('ul');
 		 ul.setAttribute('id','booksList');
 	 document.getElementById('bookShelf').appendChild(ul);
-	 booksList.forEach(renderBookList);
+	 // booksList.forEach(renderBookList);
 	 bookObjectList.forEach(createBooks);
+	 bookObjectList.forEach(addText);
 
 
 	  function renderBookList(element, index, array){
 	 	let li  = document.createElement('li');
-		li.setAttribute('class','book');
+		li.setAttribute('class','list');
 		 ul.appendChild(li);
 		li.setAttribute('id', element);
 	 }
 
 	 function createBooks(element, index, array){
-		 let liList = document.getElementsByClassName("book");
+	 	 let li  = document.createElement('li');
+		 ul.appendChild(li);
+		 li.setAttribute('id', element.id);
 		 let div = document.createElement('div');
-		 div.setAttribute('class' , 'books');
+		 div.setAttribute('class' , 'book-bg');
 		 let divBookTitle = document.createElement('div');
-		 divBookTitle.setAttribute('class', 'title')
+		 divBookTitle.setAttribute('class', 'book-cover');
 		 let p = document.createElement('p');
 		 let img = document.createElement('img');
 		 img.setAttribute('src',element.cover);
-		 let mainDiv= liList[index].appendChild(div);
-		 mainDiv.appendChild(divBookTitle).appendChild(p);
-		 mainDiv.append(img);	
-		 p.innerHTML =p.innerHTML + " " +element.author + " " + element.title ;
+		 li.appendChild(div).appendChild(divBookTitle).appendChild(img);
+		 div.appendChild(p);
+		
+	 }
+
+	 function addText(element, index, array){
+	 	let pList = document.getElementsByTagName('p');
+	 	pList[index].innerHTML = "<b>Author:</b>\t" + element.author +"<br>" +" \t<b>Title:</b>\t" 
+	 	+ element.title +"<br>" + "\t<b>Language:</b>\t" + element.language; 
 	 }
 
 
